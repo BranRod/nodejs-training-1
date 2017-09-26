2dca66a888e048e6a96540ebf547529493c746da
BranRod <brandon.v.rodriguez@pwc.com>
2017-09-26 16:38:58 -0400

    Fourth commit

---
+++ test.js
@@ -0,0 +1,6 @@
+'use strict';
+var test = {'results':[{
+  'name':'test'
+}]};
+
+console.log(test.results.length);
--- test/test.js
+++ test/test.js
@@ -1,28 +1,28 @@
 'use strict';
-const pullData = require('../pullData.js');
 const Lab = require('lab');
 const lab = exports.lab = Lab.script();
 const proxyquire =  require('proxyquire');
-var assert = require('assert');
+
+let pullData = proxyquire('../pullData', {
+  'node-fetch': function () {
+    return new Promise(function(resolve){
+      process.nextTick(function () {
+        console.error('log 2')
+        return resolve({
+          'results':[{
+            'name':'test'
+          }]
+        }
+        );
+      });
+    });
+  }});
 
 //  test the pulldata method to check if a value is returned
 lab.test('test pull data result', () => {
   return pullData()
     .then((aValue) => {
-      Lab.expect(aValue.results.length).to.equal(3);}
+      console.log(aValue);
+      //Lab.expect(aValue);
+    }
     );});
-
-
-//  Test the fetch error handler
-var fetch = proxyquire('../pullData', {
-  'node-fetch': function () {
-    process.nextTick(function () {
-      return new Promise(function() {
-        return ({'name':'test'});
-      });
-    });
-  }});
-
-fetch(function (err, res) {
-  assert(res.statusCode, 200);
-});
